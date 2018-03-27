1035973ca13907d156ea9a6fe5d67a78e82bb2a8
Matthew Hutchinson <matt@hiddenloop.com>
2018-03-27 15:11:38 +0100

    remove runner_order, bump gem version

--- lib/lolcommits/plugin/uploldz.rb
+++ lib/lolcommits/plugin/uploldz.rb
@@ -11,19 +11,9 @@ module Lolcommits
       ##
       # Initialize plugin with runner, config and set all configurable options.
       #
-      def initialize(runner: nil, config: nil)
+      def initialize(runner: nil, config: nil, name: nil)
         super
         options.concat(plugin_options)
-      end
-
-      ##
-      # Returns position(s) of when this plugin should run during the capture
-      # process. Uploading happens when a new capture is ready.
-      #
-      # @return [Array] the position(s) (:capture_ready)
-      #
-      def self.runner_order
-        [:capture_ready]
       end
 
       ##
